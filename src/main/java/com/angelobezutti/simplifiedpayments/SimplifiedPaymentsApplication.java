8357fd6ea0f0d95ce9d96f8031297cc5c4820647
Angelo Antonio Bezutti <angelo.bezutti@sysmap.com.br>
2024-03-20 22:43:51 -0300

    Add project

---
+++ src/main/java/com/angelobezutti/simplifiedpayments/SimplifiedPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.simplifiedpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelobezutti/simplifiedpayments/SimplifiedPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelobezutti.simplifiedpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
